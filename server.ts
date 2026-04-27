9701f3ea1cb73bdc6f31363d2050ffd62ad6582c
Felippe Cott <felippespe@gmail.com>
2026-04-27 08:18:13 -0300

    fix: convert PORT to number

--- server.ts
+++ server.ts
@@ -1072,7 +1072,8 @@ if (process.env.NODE_ENV !== "production") {
   });
 }
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
+
 httpServer.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
